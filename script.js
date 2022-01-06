039b32bf9982c0e3bc43c8bff0cf7bb581f5dab5
sravani <ksravani805@gmail.com>
2022-01-06 21:06:59 +0530

    linked to email dashboard

--- script.js
+++ script.js
@@ -1,15 +1,10 @@
-function validate(){
-    console.log("in js");
-    var mail= document.getElementById("email").value;
-    var pass= document.getElementById("password").value;
-    if(mail=="a@gmail.com" && pass=="1234" )
-    {
-        alert("Successfully Logged In");
-        window.location.reload("email.html");
-        return false;
-    }
-    else
-    {
-        alert("enter right credentials");
-    }
+function validate() {
+  var mail = document.getElementById("email").value;
+  var pass = document.getElementById("password").value;
+  if (mail == "a@gmail.com" && pass == "1234") {
+    window.location.reload("email.html");
+    return false;
+  } else {
+    alert("enter right credentials");
+  }
 }
