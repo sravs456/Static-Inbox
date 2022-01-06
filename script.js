9672c177b35ec9d20e671fdbb78988d1e5ee1423
sravani <ksravani805@gmail.com>
2022-01-06 17:41:51 +0530

    db

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
-function validate(){
-    var id= document.getElementById("name").value;
-    var pass= document.getElementById("password").value;
-    if(id=="sravani" && pass=="1234" )
-    {
+-- // function validate(){
+-- //     var mail= document.getElementById("email").value;
+-- //     var pass= document.getElementById("password").value;
+-- //     if(mail=="a@gmail.com" && pass=="1234" )
+-- //     {
       
-        window.location("email.html");
-        
-    }
-    else
-    {
-        alert("enter right credentials");
-    }
-}
+-- //         window.location("email.html");
+-- //         return false;
+-- //     }
+-- //     else
+-- //     {
+-- //         alert("enter right credentials");
+-- //     }
+-- // }
+
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "Test",
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
